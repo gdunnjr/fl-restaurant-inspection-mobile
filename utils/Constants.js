5b39ea1706e873e4f20b2257e946737a85f97057
G. Dunn <gdunnjr@yahoo.com>
2019-04-28 14:07:33 -0400

    change url to limit to single county

--- utils/Constants.js
+++ utils/Constants.js
@@ -1,4 +1,4 @@
-export const URL_GET_ALL_FAILED_INSPECTIONS = "http://107.22.48.117:5000/tc-health-inspection/v1/failedfirstinspection"
+export const URL_GET_ALL_FAILED_INSPECTIONS = "http://107.22.48.117:5000/tc-health-inspection/v1/failedfirstinspection/county/st-lucie"
 export const URL_GET_ALL_FAILED_INSPECTIONS_TEST = "http://107.22.48.11:5000/tc-health-inspection/v1/failedfirstinspection"
 export const ERROR_MSG_TIMEOUT = "We're sorry - an error occurred getting the data. Please check your data connection and try again."
 export const INFO_MSG_LOADING_DATA = "Loading data, almost ready..."
