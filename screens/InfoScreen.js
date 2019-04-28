afcd8314c2f6cfc6bc7ad190868e608630e348ef
G. Dunn <gdunnjr@yahoo.com>
2019-04-28 12:02:48 -0400

    Add scrollview to info page

--- screens/InfoScreen.js
+++ screens/InfoScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, Platform, Linking } from 'react-native';
+import { ScrollView, Text, View, StyleSheet, TouchableOpacity, Platform, Linking } from 'react-native';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
@@ -11,7 +11,9 @@ export default class SettingsScreen extends React.Component {
   }
 
   render() {
-    return (<View style={styles.MainContainer}>
+    return (
+    <ScrollView>
+      <View style={styles.MainContainer}>
       <Text style={{ fontWeight: 'bold' }}>{"\n"}Florida Health Inspections App</Text>
       <Text>{"\n"}Version 1.0 - 4/28/2019</Text>
       <Text>
@@ -27,9 +29,10 @@ export default class SettingsScreen extends React.Component {
         <Text style={styles.fakeLinkText}>data.tcpalm.com/restaurant-inspections</Text>
       </TouchableOpacity>
       <Text style={{ fontWeight: 'bold' }}>{"\n"}Look for an update soon that will also show restuarants with the most failures in the last 30 days and also show restaurants that passed inspections!</Text>
-      <Text style={styles.textStyle}>{"\n"}{"\n"} {'\u00A9'} 2019 Gerald C Dunn Jr</Text>
+      <Text style={styles.textStyle}>{"\n"}{"\n"} {'\u00A9'} 2019 Gerald C Dunn Jr{"\n"}</Text>
 
     </View>
+    </ScrollView>
     );
   }
 }
