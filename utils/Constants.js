16829ce155b119bc73bd209d40b9bc70876789dd
G. Dunn <gdunnjr@yahoo.com>
2019-05-29 21:26:25 -0400

    Add url for all inspections

--- utils/Constants.js
+++ utils/Constants.js
@@ -1,4 +1,10 @@
-export const URL_GET_ALL_FAILED_INSPECTIONS = "http://107.22.48.117:5000/tc-health-inspection/v1/failedfirstinspection"
+//export const URL_GET_ALL_FAILED_INSPECTIONS = "http://107.22.48.117:5000/tc-health-inspection/v1/failedfirstinspection"
+export const URL_GET_ALL_FAILED_INSPECTIONS = "http://107.22.48.117:5000/fl-restaurant-inspection/v1/failedfirstinspection"
+export const URL_GET_ALL_PASSED_INSPECTIONS = "http://107.22.48.117:5000/fl-restaurant-inspection/v1/passedfirstinspection"
+export const URL_GET_ALL_INSPECTIONS = "http://107.22.48.117:5000/fl-restaurant-inspection/v1/allfirstinspection"
+
 export const URL_GET_ALL_FAILED_INSPECTIONS_TEST = "http://107.22.48.11:5000/tc-health-inspection/v1/failedfirstinspection"
+
 export const ERROR_MSG_TIMEOUT = "We're sorry - an error occurred getting the data. Please check your data connection and try again."
+
 export const INFO_MSG_LOADING_DATA = "Loading data, almost ready..."
