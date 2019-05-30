fee1e8ccc9131d89faa920be14c898049bac3be9
G. Dunn <gdunnjr@yahoo.com>
2019-05-29 21:25:10 -0400

    Added new data formatting function for yyyy-mm-dd

--- utils/Helpers.js
+++ utils/Helpers.js
@@ -1,5 +1,5 @@
 // Accept date in DD-MMM-YY and return mm/dd/yy
-export function getParsedDate(date) {
+export function getParsedDate_dd_mmm_yy(date) {
     var m = {
       Jan: 1, Feb: 2, Mar: 3, Apr: 4, May: 5, Jun: 6,
       Jul: 7, Aug: 8, Sep: 9, Oct: 10, Nov: 11, Dec: 12
@@ -11,6 +11,20 @@ export function getParsedDate(date) {
       month_name = date[1];
       year = String(date[2]);
       formatted_date = m[month_name] + "/" + day + "/" + year;
+    }
+
+    return formatted_date;
+  }
+
+// Accept date in yyyy-mm-dd and return mm/dd/yy
+  export function getParsedDate_yyyy_mm_dd(date) {
+    formatted_date = '';
+    if (date != '') {
+      date = String(date).split('-');
+      year = String(date[0]);
+      month = String(date[1]);
+      day = String(date[2]);
+      formatted_date = month + "/" + day + "/" + year;
     }
 
     return formatted_date;
